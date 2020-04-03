74e14fb4997142b74b9b108ff0dae0df7364c7b6
Andrius Mozūraitis <andriusmozuraitis@gmail.com>
2020-04-03 16:48:55 +0300

    Simplified version

--- src/index.js
+++ src/index.js
@@ -5,21 +5,9 @@ const synth = new Tone.Synth().toMaster();
 let data = {};
 // A B C D E F G
 // 1 - 7
-
-let map = {
-  a: "F4",
-  b: "C3",
-  s: "E6"
-};
-
-// synth.triggerAttack("D3");
-
-// setTimeout(() => {
-//   synth.triggerRelease();
-// }, 500);
 document.addEventListener("keydown", e => {
-  if (!data[e.key] && map[e.key]) {
-    data[e.key] = synth.triggerAttack(map[e.key]);
+  if (!data[e.key]) {
+    data[e.key] = synth.triggerAttack("E5");
   }
 });
 
