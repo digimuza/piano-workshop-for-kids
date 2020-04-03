037199e24e7253d76806367dfc3eb1341606cc8c
Andrius Mozūraitis <andriusmozuraitis@gmail.com>
2020-04-03 17:05:38 +0300

    Added more info

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import Tone from "tone";
 const synth = new Tone.Synth().toMaster();
 
 let data = {};
-// A B C D E F G
+// C D E F G A B C
+// C1 D1 E1 F1 G1 A1 B1 C1
+// Do Re Mi Fa Sol La Si Do
 // 1 - 7
 document.addEventListener("keydown", e => {
   if (!data[e.key]) {
